d77e529fd3ad39008da212bf3d4ce70bb0e6f9fb
Evyatarr <evyatar210@gmail.com>
2022-08-19 21:50:51 +0300

    updated js syntax

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -8,35 +8,33 @@ function myFunction() {
   }
 }
 
+let messageArray = ["Full-stack Developer","Freelancer"];
+let textPosition = 0;
+let speed = 100;
 
+function typewriter() {
+  document.querySelector("#h3intro").innerHTML =
+    messageArray[0].substring(0, textPosition) + "<span>\u25ae</span>";
 
-var messageArray = ["Full-stack Developer"]
-var textPosition = 0;
-var speed = 100;
-
-typewriter = () => {
-  document.querySelector("#h3intro").
-  innerHTML = messageArray[0].substring(0,
-    textPosition) + "<span>\u25ae</span>";
-
-    if(textPosition++ != messageArray[0].length)
-      setTimeout(typewriter, speed)
-      
+  if (textPosition++ != messageArray[0].length) setTimeout(typewriter, speed);
 }
 
-window.addEventListener("load", typewriter)
-
-
+window.addEventListener("load", typewriter);
 
 
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -47,4 +45,4 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+}
