2bf5e6a9fa3564674369470236037fca27485f47
Evyatarr <evyatar210@gmail.com>
2022-08-19 12:06:39 +0300

    margin fixes. added typewriter effect in js

--- script/script.js
+++ script/script.js
@@ -6,4 +6,19 @@ function myFunction() {
     x.className = "topnav";
   }
 }
- 
+
+var messageArray = ["Full-stack Developer"]
+var textPosition = 0;
+var speed = 100;
+
+typewriter = () => {
+  document.querySelector("#h3intro").
+  innerHTML = messageArray[0].substring(0,
+    textPosition) + "<span>\u25ae</span>";
+
+    if(textPosition++ != messageArray[0].length)
+      setTimeout(typewriter, speed)
+      
+}
+
+window.addEventListener("load", typewriter)
