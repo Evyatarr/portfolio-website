5f4abd13ecf988c282faa0199b2d73945606199d
Evyatarr <evyatar210@gmail.com>
2022-08-19 10:48:41 +0300

    updated to only h1 line zsh:1: command not found: push

--- script/script.js
+++ script/script.js
@@ -1,8 +1,9 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  } 
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+ 
