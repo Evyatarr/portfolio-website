29921c07e6040ad0bc8372c45f41165726c42b21
Evyatarr <evyatar210@gmail.com>
2022-08-19 16:33:13 +0300

    Added scroll up button

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -6,6 +7,8 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+
 
 var messageArray = ["Full-stack Developer"]
 var textPosition = 0;
@@ -22,3 +25,26 @@ typewriter = () => {
 }
 
 window.addEventListener("load", typewriter)
+
+
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
